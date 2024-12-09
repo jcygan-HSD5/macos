b1b6c0d46aacaab6208614506dc5538f932dbe81
jcygan-HSD5 <168202316+jcygan-HSD5@users.noreply.github.com>
2024-12-09 14:02:50 -0600

    Update swift-reboot.sh

--- swift-reboot.sh
+++ swift-reboot.sh
@@ -21,7 +21,7 @@
         --button1text "OK" \
         --button1action "quit" \
         --button2text "Reboot Now" \
-        --button2action "run_command:/usr/bin/sudo /sbin/shutdown -r now"
+        --button2action "run_command:/usr/bin/sudo reboot now"
 #else
 #    echo "Uptime is not more than 14 days. No dialog displayed."
 #fi
