0e6e91b8ce70b2cdb64ba9688888ef50e6d21335
jcygan-HSD5 <168202316+jcygan-HSD5@users.noreply.github.com>
2024-12-09 13:53:06 -0600

    Update swift-reboot.sh

--- swift-reboot.sh
+++ swift-reboot.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 # Get the current uptime in days
-days=$(uptime | awk -F'( |,)' '{for(i=1;i<=NF;i++){if($i~/day/) {print $(i-1)}}}')
+#days=$(uptime | awk -F'( |,)' '{for(i=1;i<=NF;i++){if($i~/day/) {print $(i-1)}}}')
 
-if [ -z "$days" ]; then
-    days=0
-fi
+#if [ -z "$days" ]; then
+#    days=0
+#fi
 
-echo "Current uptime in days: $days"
+#echo "Current uptime in days: $days"
 
 # Check if uptime is greater than 14 days
-if [ "$days" -gt 0 ]; then
+#if [ "$days" -gt 0 ]; then
     echo "Uptime is more than 14 days. Triggering reboot dialog..."
     /usr/local/bin/dialog \
         --title "Recommended Reboot" \
@@ -22,6 +22,6 @@ if [ "$days" -gt 0 ]; then
         --button1action "quit" \
         --button2text "Reboot Now" \
         --button2action "run_command:sudo reboot now"
-else
-    echo "Uptime is not more than 14 days. No dialog displayed."
-fi
+#else
+#    echo "Uptime is not more than 14 days. No dialog displayed."
+#fi
